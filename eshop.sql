1366888ad2d5b204aad5fcb3cebe2a710e899031
nhatminh280 <minh2508tv@gmail.com>
2025-11-12 02:51:07 +0700

    update docker file and fix recommender bug

---
+++ eshop.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict uVUv7vgE6Lp9WMc826Dj9gZxhCn3dP0q9FE9l35XYUrQKxcHhy6v70u4feDzQdM
+
+-- Dumped from database version 16.10 (Debian 16.10-1.pgdg12+1)
+-- Dumped by pg_dump version 17.6 (Ubuntu 17.6-2.pgdg24.04+1)
+
+-- Started on 2025-11-10 18:43:07 +07
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2025-11-10 18:43:10 +07
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict uVUv7vgE6Lp9WMc826Dj9gZxhCn3dP0q9FE9l35XYUrQKxcHhy6v70u4feDzQdM
+
